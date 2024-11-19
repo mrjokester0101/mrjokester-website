a9bf45f8f724803a1f11764d5470efea026e22ac
MrJokester <mrjokester01010@gmail.com>
2024-11-19 21:29:27 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-
+import MainButtons from "../components/MainButtons.js";
 const Home = () => {
   return (
     <div className={styles.all}>
@@ -17,23 +17,7 @@ const Home = () => {
           </h1>
           <p className={styles.description}>Main Page</p>
   
-        <div className={styles.buttons}>
-          <button
-            className={styles.button}
-            onClick={() => window.location.href = 'https://mrjokester.vercel.app/'}>
-            <h2>Main Page</h2>
-          </button>
-          <button
-            className={styles.button}
-            onClick={() => window.location.href = 'https://mrjokester.vercel.app/me'}>
-            <h2>About Me</h2>
-          </button>
-          <button
-            className={styles.button}
-            onClick={() => window.location.href = 'https://mrjokester.vercel.app/bdfd'}>
-            <h2>BDFD Snippets</h2>
-          </button>
-        </div>
+        <MainButtons/>
               
         <div className={styles.grid}>
           <div className={styles.card}>
