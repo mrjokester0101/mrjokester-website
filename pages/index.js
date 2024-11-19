d2cadda422f9c74eb7e6271db14646769f0301e1
MrJokester <mrjokester01010@gmail.com>
2024-11-19 21:33:03 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import MainButtons from "../components/MainButtons.js";
+
 const Home = () => {
   return (
     <div className={styles.all}>
