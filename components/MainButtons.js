7a5583b28dc828d5687c54c9979980ae8fe814bc
MrJokester <mrjokester01010@gmail.com>
2024-11-19 21:21:54 +0200

    Update MainButtons.js

--- components/MainButtons.js
+++ components/MainButtons.js
@@ -1,1 +1,27 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
+const Home = () => {
+  return (
+      <div className={styles.buttons}>
+        <button
+          className={styles.button}
+          onClick={() => window.location.href = 'https://mrjokester.vercel.app/'}>
+          <h2>Main Page</h2>
+        </button>
+        <button
+          className={styles.button}
+          onClick={() => window.location.href = 'https://mrjokester.vercel.app/me'}>
+          <h2>About Me</h2>
+        </button>
+        <button
+          className={styles.button}
+          onClick={() => window.location.href = 'https://mrjokester.vercel.app/bdfd'}>
+        <h2>BDFD Snippets</h2>
+      </button>
+    </div>
+  );
+};
+
+export default Home;
