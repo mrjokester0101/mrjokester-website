55b6612c6251b8fa9be079ae674b36f3933759a8
Captain Jokester <mrjokester01010@gmail.com>
2026-02-01 09:33:51 +0200

    Fixed 2 missing </p>.

--- pages/me.js
+++ pages/me.js
@@ -26,9 +26,9 @@ const Home = () => {
               <h2>1. Introduction</h2>
                 <p>Hello, I am Captain Joker . I am a 14 years old boy. I like coding, playing and learning as i go. I live in Bulgaria with my family and i am jobless.</p>
               <h2>2. Technical Skills</h2>
-                <p>I am pretty good with the windows operating system. I have explored a few Linux distros, such as Mint, Arch, AntiX and Fedora. Even as far creating 2 of my own, such as arch-based and LFS with the names Veorc. </p>
-                <p>I can code in BDScript very good and make pretty good JSONs. I can't create very good HTML & CSS. And I am a beginner at JavaScript, Python, Automate Flows, Windows CMD and PowerShell.
-                <p>I have contributed in a few wikipedia pages. Not as this user tho, I am staying anomylous as what user I had contributed with.
+                <p>I am pretty good with the windows operating system. I have explored a few Linux distros, such as Mint, Arch, AntiX and Fedora. Even as far creating 2 of my own, such as arch-based and LFS with the names Veorc.</p>
+                <p>I can code in BDScript very good and make pretty good JSONs. I can't create very good HTML & CSS. And I am a beginner at JavaScript, Python, Automate Flows, Windows CMD and PowerShell.</p>
+                <p>I have contributed in a few wikipedia pages. Not as this user tho, I am staying anomylous as what user I had contributed with.</p>
               <h2>3. Disadvantages</h2>
                 <p>I have schizoprenhia, social anxiety, can't explain & design very well and can't focus that good.</p> 
               <h2>3. Contact Information</h2>
