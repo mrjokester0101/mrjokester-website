a0c8cd8247eb6d14510d2779dc0221d077c3be8b
Captain Jokester <mrjokester01010@gmail.com>
2026-02-01 09:28:31 +0200

    Updated me.js (about me) to state more information and for it to be written better.

--- pages/me.js
+++ pages/me.js
@@ -24,18 +24,15 @@ const Home = () => {
           <div className={styles.aboutbox}>
             <div className={styles.aboutme}>
               <h2>1. Introduction</h2>
-                <p> • Name: MrJokester</p>
-                <p> • Age: 13 years old</p>
-                <p> • Gender: Male</p>
-                <p> • Hobbies: Coding, Chatting, Playing, Learning</p>
-                <p> • Country: Bulgaria</p>
-                <p> • Job: Jobless</p>
+                <p>Hello, I am Captain Joker . I am a 14 years old boy. I like coding, playing and learning as i go. I live in Bulgaria with my family and i am jobless.</p>
               <h2>2. Technical Skills</h2>
-                <p> • Beginner: Javascript, Python, CSS, HTML & Flows</p>
-                <p> • Very Good: Json & BDScript</p>
+                <p>I am pretty good with the windows operating system. I have explored a few Linux distros, such as Mint, Arch, AntiX and Fedora. Even as far creating 2 of my own, such as arch-based and LFS with the names Veorc. </p>
+                <p>I can code in BDScript very good and make pretty good JSONs. I can't create very good HTML & CSS. And I am a beginner at JavaScript, Python, Automate Flows, Windows CMD and PowerShell.
+                <p>I have contributed in a few wikipedia pages. Not as this user tho, I am staying anomylous as what user I had contributed with.
+              <h2>3. Disadvantages</h2>
+                <p>I have schizoprenhia, social anxiety, can't explain & design very well and can't focus that good.</p> 
               <h2>3. Contact Information</h2>
-                <p> • Email: mrjokester01010@gmail.com</p>
-                <p> • Discord: mrjokester0101</p>
+                <p>You can contact me on mrjokester01010@gmail.com or with mrjokester0101 in discord</p>
             </div>
           </div>
           
